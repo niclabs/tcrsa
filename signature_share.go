7e6afdb760445715c6c4eb3989ce7ae59e49145b
Eduardo <eduardo@niclabs.cl>
2019-04-04 12:56:27 -0300

    Startinf with signature joining

--- signature_share.go
+++ signature_share.go
@@ -1,8 +1,25 @@
 package main
 
+import "fmt"
+
 type SignatureShare struct {
 	Xi []byte
 	c []byte
 	z []byte
 	id uint16
+}
+
+
+type SignatureShares []SignatureShare
+
+
+// Joins the signatures of the document provided.
+func (s SignatureShares) JoinSignatures(document []byte, info *KeyMeta) ([]byte, error) {
+	if document == nil {
+		return []byte{}, fmt.Errorf("document is nil")
+	}
+	if info == nil {
+		return []byte{}, fmt.Errorf("key metainfo is nil")
+	}
+
 }
